eea5736db7baa97cce27c13746629bd53005c569
c9s <cornelius.howl@gmail.com>
2012-02-07 03:23:05 +0800

    Add InstallCommand

--- src/PhpBrew/Command/InstallCommand.php
+++ src/PhpBrew/Command/InstallCommand.php
@@ -1,5 +1,6 @@
 <?php
 namespace PhpBrew\Command;
+use Exception;
 
 class InstallCommand extends \CLIFramework\Command
 {
@@ -7,6 +8,14 @@ class InstallCommand extends \CLIFramework\Command
 
     public function execute($version)
     {
+        $versions = \PhpBrew\PhpStas::getVersions();
+        if( ! isset($versions[$version] ) )
+            throw new Exception("Version $version not found.");
+
+        $url = $versions[ $version ];
+
+        var_dump( $url ); 
+        
 
     }
 }
