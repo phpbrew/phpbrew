43665d175732e0e1cf0dd7cb32e48c751ea53712
c9s <cornelius.howl@gmail.com>
2012-02-07 03:27:24 +0800

    Download php source

--- src/PhpBrew/Command/InstallCommand.php
+++ src/PhpBrew/Command/InstallCommand.php
@@ -8,14 +8,27 @@ class InstallCommand extends \CLIFramework\Command
 
     public function execute($version)
     {
+        $logger = $this->getLogger();
         $versions = \PhpBrew\PhpStas::getVersions();
         if( ! isset($versions[$version] ) )
             throw new Exception("Version $version not found.");
 
         $url = $versions[ $version ];
 
-        var_dump( $url ); 
-        
+        $home = getenv('HOME') . DIRECTORY_SEPARATOR . '.phpbrew';
+        if( ! file_exists($home) )
+            mkdir( $home );
+        chdir( $home );
+
+        $parts = parse_url($url);
+        $basename = basename( $parts['path'] );
+
+        $logger->info("Downloading $url");
+        system( 'curl -# -O ' . $url );
+
+
+
+
 
     }
 }
