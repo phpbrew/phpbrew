257bfd5a7be779605fedbd46a451660d302448c3
c9s <cornelius.howl@gmail.com>
2012-02-07 03:18:40 +0800

    list known versions

--- src/PhpBrew/Command/KnownCommand.php
+++ src/PhpBrew/Command/KnownCommand.php
@@ -8,23 +8,7 @@ class KnownCommand extends \CLIFramework\Command
 
     public function execute()
     {
-        $baseUrl = 'http://downloads.php.net/stas/';
-        $html = file_get_contents($baseUrl);
-        $dom = new DOMDocument;
-        $dom->loadHtml( $html );
-
-        $items = $dom->getElementsByTagName('a');
-        $versions = array();
-        foreach( $items as $item ) 
-        {
-
-            $href = $item->getAttribute('href');
-            if( preg_match('/php-(.*?)\.tar\.bz2$/' , $href , $regs ) ) {
-                $version = $regs[1];
-                $link = $baseUrl . $href;
-                $versions[$version] = $link;
-            }
-        }
+        $versions = \PhpBrew\PhpStas::getVersions();
 
         // var_dump( $versions ); 
         echo "Available versions:\n";
