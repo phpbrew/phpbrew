b74490c5abf8942c5cd3b028e8ab903336cf87d5
c9s <cornelius.howl@gmail.com>
2012-04-30 00:34:16 +0800

    bump version for bug fix

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application 
 {
     const name = 'phpbrew';
-    const version = '1.3.3';
+    const version = '1.3.4';
 
     public function init()
     {
