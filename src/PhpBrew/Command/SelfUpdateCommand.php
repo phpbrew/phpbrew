3314027f06609171549c888b07df253eda7f16d4
c9s <cornelius.howl@gmail.com>
2012-06-27 19:06:58 +0800

    add brief and usage to self-update command.

--- src/PhpBrew/Command/SelfUpdateCommand.php
+++ src/PhpBrew/Command/SelfUpdateCommand.php
@@ -7,9 +7,11 @@ use CLIFramework\Command;
 class SelfUpdateCommand extends Command
 {
 
-    public function brief() { return 'self-update'; }
+    public function usage() { return 'phpbrew self-update [branch name]'; }
 
-    public function execute()
+    public function brief() { return 'self-update, default to master version'; }
+
+    public function execute($branch = 'master')
     {
         global $argv;
         $script = realpath( $argv[0] );
@@ -18,13 +20,12 @@ class SelfUpdateCommand extends Command
         }
 
         // fetch new version phpbrew
-        $this->getLogger()->info("Fetching phpbrew to $script...");
+        $this->logger->info("Updating phpbrew $script from $branch...");
 
-        $url = 'https://raw.github.com/c9s/phpbrew/master/phpbrew';
+        $url = "https://raw.github.com/c9s/phpbrew/$branch/phpbrew";
         system("curl -# -L $url > $script") == 0 or die('Update failed.');
 
-        $this->getLogger()->info("Version updated.");
-
+        $this->logger->info("Version updated.");
         system( $script . ' --version' );
     }
 }
