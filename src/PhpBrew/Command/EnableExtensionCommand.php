f2b966ac625eba3ebad5280b0dbb57b7037b0c2c
c9s <cornelius.howl@gmail.com>
2012-06-28 13:19:21 +0800

    check loaded extension

--- src/PhpBrew/Command/EnableExtensionCommand.php
+++ src/PhpBrew/Command/EnableExtensionCommand.php
@@ -13,7 +13,11 @@ class EnableExtensionCommand extends \CLIFramework\Command
 
     public function execute($extensionName)
     {
-        Utils::create_extension_config($extensionName);
-        $this->logger->info("$extensionName is enabled now.");
+        if( ! extension_loaded($extensionName) ) {
+            Utils::create_extension_config($extensionName);
+            $this->logger->info("$extensionName is enabled now.");
+        } else {
+            $this->logger->info("$extensionName is already enabled.");
+        }
     }
 }
