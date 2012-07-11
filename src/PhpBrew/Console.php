98385e9929467dea9aa59865c152ecb7f289c719
c9s <cornelius.howl@gmail.com>
2012-07-11 17:18:29 +0800

    bump version to 1.5.2

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application 
 {
     const name = 'phpbrew';
-    const version = '1.5.0';
+    const version = '1.5.2';
 
     public function init()
     {
