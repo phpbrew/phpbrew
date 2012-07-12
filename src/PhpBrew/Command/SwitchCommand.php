057b620f9b924e34c4d5d0b9920365c0c6e052a1
c9s <cornelius.howl@gmail.com>
2012-07-12 09:36:38 +0800

    Update switch command

--- src/PhpBrew/Command/SwitchCommand.php
+++ src/PhpBrew/Command/SwitchCommand.php
@@ -5,10 +5,10 @@ use Exception;
 
 class SwitchCommand extends \CLIFramework\Command
 {
-    public function brief() { return 'switch php version as default.'; }
+    public function brief() { return 'switch default php version.'; }
 
     public function execute($version = null)
     {
-        throw new Exception("You should not see this, please check if bashrc is sourced in your shell.");
+        throw new Exception("You should not see this, please check if phpbrew bashrc is sourced in your shell.");
     }
 }
