9ae4614c5f5552a5765bed499491225d04f6b20d
c9s <cornelius.howl@gmail.com>
2012-09-03 14:24:27 +0800

    recompile and bump version

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application 
 {
     const name = 'phpbrew';
-    const version = '1.6.0';
+    const version = '1.6.1';
 
     public function init()
     {
