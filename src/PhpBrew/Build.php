7faf02318b11b21deff8019191a60fdf287e7f9e
c9s <cornelius.howl@gmail.com>
2013-03-04 22:23:58 +0800

    Add Build class.

---
+++ src/PhpBrew/Build.php
@@ -0,0 +1,18 @@
+<?php
+namespace PhpBrew;
+
+
+
+/**
+ * A build object contains version information, 
+ * variant configuration, paths and an build identifier (BuildId)
+ */
+class Build
+{
+    public $version;
+    public $variants;
+
+}
+
+
+
