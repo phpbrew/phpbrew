5b3dc59271b51873b6f477045daa2e6cbe018aac
c9s <cornelius.howl@gmail.com>
2013-03-04 22:29:13 +0800

    implement build class

--- src/PhpBrew/Build.php
+++ src/PhpBrew/Build.php
@@ -9,8 +9,58 @@ namespace PhpBrew;
  */
 class Build
 {
+    const ENV_PRODUCTION;
+    const ENV_DEVELOPMENT;
+
     public $version;
-    public $variants;
+
+    public $variants = array();
+
+    public $phpEnvironment = self::ENV_DEVELOPMENT;
+
+    public function __construct()
+    {
+
+    }
+
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+
+    public function addVariant($name, $value = null)
+    {
+        $this->variants[$name] = $value ?: true;
+    }
+
+    public function setVariants($variants)
+    {
+        $this->variants = $variants;
+    }
+
+    public function removeVariant($variantName)
+    {
+        unset($this->variants[$variantName]);
+    }
+
+
+
+    /**
+     * Returns a build identifier.
+     */
+    public function getIdentifier()
+    {
+        $names = array('php');
+        $names[] = $this->version;
+
+        if($this->phpEnvironment === self::ENV_PRODUCTION ) {
+            $names[] = 'prod';
+        } elseif($this->phpEnvironment === self::ENV_DEVELOPMENT ) {
+            $names[] = 'dev';
+        }
+
+
+    }
 
 }
 
