36da05fd8ac629caaf73f916f007999323d551e8
c9s <cornelius.howl@gmail.com>
2013-03-09 18:21:43 +0800

    Bump version to 1.8.6

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'phpbrew';
-    const version = '1.8.5';
+    const version = '1.8.6';
 
     public function init()
     {
