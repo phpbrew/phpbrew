a6ffd50207cccbf9d9ba6943ceace677059d5881
c9s <cornelius.howl@gmail.com>
2013-03-16 00:07:21 +0800

    Bump version to 1.9.2

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'phpbrew';
-    const version = '1.9.1';
+    const version = '1.9.2';
 
     public function init()
     {
