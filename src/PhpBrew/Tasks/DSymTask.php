cc363755a93062fb13367c77c0b2a46280dc16e8
c9s <cornelius.howl@gmail.com>
2013-03-15 23:11:47 +0800

    Add DSYmTask

---
+++ src/PhpBrew/Tasks/DSymTask.php
@@ -0,0 +1,28 @@
+<?php
+namespace PhpBrew\Tasks;
+
+class DSymTask extends BaseTask
+{
+
+    // Fix php.dSYM
+    /* Check if php.dSYM exists */
+    public function check($build) 
+    {
+        $phpbin = $build->getBinPath() . DIRECTORY_SEPARATOR . 'php';
+        $dSYM = $build->getBinPath() . DIRECTORY_SEPARATOR . 'php.dSYM';
+        return ! file_exists($phpbin) && file_exists($dSYM);
+    }
+
+    public function patch($build, $options) 
+    {
+        if ( $this->check($build) ) {
+            $this->logger->info("---> Moving php.dSYM to php ");
+            if ( ! $options->dryrun ) {
+                $php = $buildPrefix . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'php';
+                rename( $dSYM , $php );
+            }
+        }
+    }
+}
+
+
