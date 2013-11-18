3fa527ad3dc0ace31e9257da5a73c5eff37a9fad
c9s <cornelius.howl@gmail.com>
2013-11-18 17:55:09 +0800

    Adding fpm subcommand.

--- phpbrew.sh
+++ phpbrew.sh
@@ -80,6 +80,12 @@ function phpbrew ()
                 __phpbrew_reinit $2
             fi
             ;;
+        fpm)
+            PHPFPM_BIN=$PHPBREW_ROOT/$PHPBREW_PHP/sbin/php-fpm
+            echo "Starting php-fpm..."
+            $PHPFPM_BIN --php-ini $PHPBREW_ROOT/$PHPBREW_PHP/etc/php.ini \
+                    --php-fpm $PHPBREW_ROOT/$PHPBREW_PHP/etc/php-fpm.conf
+            echo "Done"
         off)
             unset PHPBREW_PHP
             unset PHPBREW_PATH
