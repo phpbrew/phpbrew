7329b860dd03506375d3989dafa4bd18e3932afe
c9s <cornelius.howl@gmail.com>
2013-11-18 22:09:12 +0800

    Checking in changes prior to tagging of version 1.8.25.

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'phpbrew';
-    const VERSION = "1.8.24";
+    const VERSION = "1.8.25";
 
     public function init()
     {
