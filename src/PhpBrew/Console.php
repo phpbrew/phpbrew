56b95523652781613f29d12c235e18f487dd8e14
c9s <cornelius.howl@gmail.com>
2013-12-02 03:46:46 +0800

    Checking in changes prior to tagging of version 1.10.2.

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = "1.10.1";
+    const VERSION = "1.10.2";
 
     public function init()
     {
