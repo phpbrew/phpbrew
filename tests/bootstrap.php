1b6a319715c7b8ee82c30a09c7c0a214d2af5fe9
c9s <cornelius.howl@gmail.com>
2013-12-02 20:18:00 +0800

    Fix phpunit bootstrap script

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,2 @@
 <?php
-require 'tests/helpers.php';
-require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
-$loader = new \Universal\ClassLoader\BasePathClassLoader( array(
-    dirname(dirname(__FILE__)) . '/src', 
-    dirname(dirname(__FILE__)) . '/vendor/pear' 
-));
-$loader->register();
+require 'vendor/autoload.php';
