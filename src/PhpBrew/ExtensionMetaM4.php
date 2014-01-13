406d9c9b4fc613398acda9387e9b9911a6cd2b75
Márcio Almada <marcio3w@gmail.com>
2014-01-13 03:22:11 -0300

    ExtensionMetaM4 is now guessing if extension is zend

--- src/PhpBrew/ExtensionMetaM4.php
+++ src/PhpBrew/ExtensionMetaM4.php
@@ -7,23 +7,24 @@ use PEARX\Utils as PEARXUtils;
 class ExtensionMetaM4 extends ExtensionMetaAbstraction implements ExtensionMetaInterface
 {
 
-    protected $meta;
+    protected $m4;
+    protected $name;
+    protected $is_zend;
 
     public function __construct($m4)
     {
         if(!file_exists($m4)) {
             throw new \Exception("Error loading m4 file: {$m4}");
         }
-        $this->meta = file_get_contents($m4);
-        preg_match_all('#(?<=PHP_NEW_EXTENSION\()\w+#s', $this->meta, $matches);
+        $this->m4 = file_get_contents($m4);
+        preg_match_all('#(?<=PHP_NEW_EXTENSION\()\w+#s', $this->m4, $matches);
         $this->name = $matches[0][0];
+        $this->is_zend = (1 === preg_match('/PHP_NEW_EXTENSION\(([^,]*,){6}\s*yes/', $this->m4));
     }
 
-    /**
-     * @todo How to know if extension is zend when using config.m4 meta?
-     */
     public function isZend()
     {
+        return $this->is_zend;
     }
 
     public function getName()
