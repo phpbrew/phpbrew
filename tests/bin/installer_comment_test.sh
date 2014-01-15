6850b7e4ee3151fe6c8fdac1d9fc216d83576eb5
chadrien <chadrien@chadrien.fr>
2014-01-15 20:46:45 +0100

    Added function do determine where phpbrew should be installed

---
+++ tests/bin/installer_comment_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source bin/installer
+
+set +o errtrace
+set +o errexit
+
+## set prefix to $HOME/.phpbrew if installed as normal user
+phpbrew_install_set_defaults # env[phpbrew_prefix]=~/\/.phpbrew$/
+
+## set prefix to /usr/local if installed as root
+## trick to "mock" UID: http://lists.gnu.org/archive/html/bug-bash/2000-09/msg00019.html
+env - UID=0 /usr/bin/env sh -c "source bin/installer; phpbrew_install_set_defaults; echo \$phpbrew_prefix" # match=~/\/usr\/local/
