c14ed75a589851f2bbed3cd4a97cd23868c8018b
Hadrien <hadrien@occitech.fr>
2014-01-16 14:15:11 +0100

    Better test for setting $HOME as prefix for standard user

--- tests/bin/installer_comment_test.sh
+++ tests/bin/installer_comment_test.sh
@@ -6,8 +6,8 @@ set +o errtrace
 set +o errexit
 
 ## set prefix to $HOME/.phpbrew if installed as normal user
-phpbrew_install_set_defaults # env[phpbrew_prefix]=~/\/.phpbrew$/
+sh -c "source bin/installer; phpbrew_install_set_defaults; if [[ \"\$HOME\" = \"\$phpbrew_prefix\" ]]; then exit 0; else exit 1; fi" # status=0
 
 ## set prefix to /usr/local if installed as root
 ## trick to "mock" UID: http://lists.gnu.org/archive/html/bug-bash/2000-09/msg00019.html
-env - UID=0 /usr/bin/env sh -c "source bin/installer; phpbrew_install_set_defaults; echo \$phpbrew_prefix" # match=~/\/usr\/local/
+env - UID=0 /usr/bin/env sh -c "source bin/installer; phpbrew_install_set_defaults; echo \$phpbrew_prefix" # match=/\/usr\/local/
