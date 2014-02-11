54ef6b80bb569a1d2850857f355bf94befd141fc
chadrien <chadrien@chadrien.fr>
2014-02-11 21:50:04 +0100

    Upgrade can get current php major version - refs #171

--- sh/tests/scripts/upgrade_comment_test.sh
+++ sh/tests/scripts/upgrade_comment_test.sh
@@ -8,3 +8,7 @@ unset PHPBREW_PHP
 is_phpbrew_active # status=1
 PHPBREW_PHP=$oldPhp
 is_phpbrew_active # status=0
+
+## can get current php version
+PHPBREW_PHP=php-5.5.7
+get_current_php_major_version # match=/^php-5.5$/
