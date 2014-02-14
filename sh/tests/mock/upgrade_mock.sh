1d39100fbf76e76adaa3693c1b03d04fce866d07
Hadrien <hadrien@occitech.fr>
2014-02-14 13:54:41 +0100

    Use more generic functions

--- sh/tests/mock/upgrade_mock.sh
+++ sh/tests/mock/upgrade_mock.sh
@@ -2,16 +2,16 @@
 
 
 cat() {
-  if [[ "$1" = "$PHPBREW_HOME/php/$PHPBREW_PHP/phpbrew.variants" ]]
+  if test "$1" = "$PHPBREW_HOME/php/$PHPBREW_PHP/phpbrew.variants"
   then
     echo 'a:3:{s:16:"enabled_variants";a:11:{s:3:"pdo";b:1;s:5:"mysql";b:1;s:5:"pgsql";s:31:"/opt/local/lib/postgresql92/bin";s:3:"fpm";b:1;s:6:"sqlite";b:1;s:5:"pcntl";b:1;s:5:"posix";b:1;s:7:"gettext";b:1;s:4:"intl";b:1;s:7:"openssl";b:1;s:7:"default";b:1;}s:17:"disabled_variants";a:1:{s:5:"pgsql";b:1;}s:13:"extra_options";a:4:{i:0;s:25:"--with-icu-dir=/opt/local";i:1;s:24:"--with-mcrypt=/opt/local";i:2;s:23:"--enable-maintainer-zts";i:3;s:14:"--enable-debug";}}'
   else
-    command -p cat $1
+    command -p cat $*
   fi
 }
 
 phpbrew() {
-  if [[ "$1" = "known" ]]
+  if test "$1" = "known"
   then
     echo "Available stable versions:
     5.5 versions:    5.5.9, 5.5.8, 5.5.7, 5.5.6, 5.5.5, 5.5.4, 5.5.3, 5.5.2
