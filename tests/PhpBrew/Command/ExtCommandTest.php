ddf795ae5285341a9b741b66522844ef13903504
Márcio Almada <marcio3w@gmail.com>
2014-06-24 03:22:14 -0300

    add failing test for `phpbrew ext` command output this command broke after #242 merge, this test will prevent future regressions improves #253

---
+++ tests/PhpBrew/Command/ExtCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class ExtCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function testOutput()
+    {
+        ob_start();
+        $console = new PhpBrew\Console;
+        $console->run( [null, 'ext'] );
+        $output = ob_get_clean();
+
+        $this->assertEquals(1, preg_match_all('#Loaded extensions:#', $output));
+        $this->assertRegExp('#\[\*\]#', $output);
+        $this->assertEquals(count(get_loaded_extensions()), preg_match_all('#\[\*\]#', $output));
+        $this->assertEquals(1, preg_match_all('#Available extensions:#', $output));
+        $this->assertRegExp('#\[ \]#', $output);
+    }
+
+    /**
+     * @outputBuffering enabled
+     */
+    public function testOutputWithDifferentPHPVersion()
+    {
+        $console = new PhpBrew\Console;
+        $console->run( [null, 'ext', '--php', '0.0'] );
+
+        $this->expectOutputRegex('#PHP version is different from current active version.#');
+    }
+}
