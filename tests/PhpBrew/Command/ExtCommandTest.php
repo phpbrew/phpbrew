1a99f9de6413866800eed909c2286053aaa97ec6
Márcio Almada <marcio3w@gmail.com>
2014-06-24 03:50:16 -0300

    better test for `ext` command output since binary strings cant be tested with regex #242 #253

--- tests/PhpBrew/Command/ExtCommandTest.php
+++ tests/PhpBrew/Command/ExtCommandTest.php
@@ -7,23 +7,33 @@ class ExtCommandTest extends PHPUnit_Framework_TestCase
         ob_start();
         $console = new PhpBrew\Console;
         $console->run( array(null, 'ext') );
-        $output = ob_get_clean();
 
-        $this->assertEquals(1, preg_match_all('#Loaded extensions:#', $output));
-        $this->assertRegExp('#\[\*\]#', $output);
-        $this->assertEquals(count(get_loaded_extensions()), preg_match_all('#\[\*\]#', $output));
-        $this->assertEquals(1, preg_match_all('#Available extensions:#', $output));
-        $this->assertRegExp('#\[ \]#', $output);
+        $output = explode("\n", ob_get_clean());
+
+        $extensions = count(get_loaded_extensions());
+
+        $this->assertSame('', array_pop($output)); // must end with a blank line
+        
+        foreach ($output as $line => $buffer) {
+            if($line === 0) { // title
+                $this->assertContains('Loaded extensions:', $buffer);
+            } else if($line <= $extensions) { // loaded extensions
+                $this->assertContains('[*]', $buffer);
+            } else if($line == $extensions + 1) { // title
+                $this->assertContains('Available extensions:', $buffer);
+            } else { // available extensions
+                $this->assertContains('[ ]', $buffer);
+            }
+        }
     }
 
-    /**
-     * @outputBuffering enabled
-     */
     public function testOutputWithDifferentPHPVersion()
     {
+        ob_start();
         $console = new PhpBrew\Console;
         $console->run( array(null, 'ext', '--php', '0.0') );
+        $output = ob_get_clean();
 
-        $this->expectOutputRegex('#PHP version is different from current active version.#');
+        $this->assertContains('PHP version is different from current active version.', $output);
     }
 }
