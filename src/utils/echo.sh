9c853cef73d906cde46e45e24dbfb47b05989269
chadrien <chadrien@chadrien.fr>
2014-07-01 19:46:01 +0200

    Added utils for nice and colored display

---
+++ src/utils/echo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo_info() {
+  printf "\033[00;34m$1\033[0m"
+}
+
+echo_user() {
+  printf "\033[0;33m$1\033[0m"
+}
+
+echo_success() {
+  printf "\033[00;32m$1\033[0m"
+}
+
+echo_fail() {
+  printf "\033[00;31m$1\033[0m"
+}
