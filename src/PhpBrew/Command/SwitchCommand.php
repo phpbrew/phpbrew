c8c3dbe2caba9616d1269694ee628d905be5d26b
c9s <yoanlin93@gmail.com>
2014-07-03 18:16:39 +0800

    Implement basic argument completions

--- src/PhpBrew/Command/SwitchCommand.php
+++ src/PhpBrew/Command/SwitchCommand.php
@@ -3,6 +3,14 @@ namespace PhpBrew\Command;
 
 class SwitchCommand extends VirtualCommand
 {
+
+    public function arguments($args) {
+        $args->add('installed php')
+            ->validValues('PhpBrew\\Config::getInstalledPhpVersions')
+            ;
+    }
+
+
     public function brief()
     {
         return 'switch default php version.';
--- src/PhpBrew/Command/UseCommand.php
+++ src/PhpBrew/Command/UseCommand.php
@@ -3,6 +3,13 @@ namespace PhpBrew\Command;
 
 class UseCommand extends VirtualCommand
 {
+
+    public function arguments($args) {
+        $args->add('installed php')
+            ->validValues('PhpBrew\\Config::getInstalledPhpVersions')
+            ;
+    }
+
     public function brief()
     {
         return 'use php, switch version temporarily';
