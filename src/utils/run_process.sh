cbe36f29e69d3dddee09b637d35d9774456eadcf
chadrien <chadrien@chadrien.fr>
2014-07-05 10:56:57 +0200

    Tweaked run_process display and added logging option

--- src/utils/run_process.sh
+++ src/utils/run_process.sh
@@ -2,18 +2,27 @@
 
 function run_process() {
   local pid
+  local log_file='/dev/null'
+
+  local OPTIND
+  while getopts 'l:' option; do
+    case "${option}" in
+      l)
+        log_file=${OPTARG}
+        shift 2
+        ;;
+    esac
+  done
 
   set +o monitor
 
-  $@ > /dev/null 2>&1 &
+  $@ >> ${log_file} 2>&1 &
   pid=$!
-
-  printf "[ ]"
 
   trap "kill ${pid} 2> /dev/null" EXIT
   while kill -0 ${pid} 2> /dev/null
   do
-    printf "\033[0;33\b\b#\033[0m ]"
+    printf "\033[0;33\#\033[0m"
     sleep 1
   done
   trap - EXIT
