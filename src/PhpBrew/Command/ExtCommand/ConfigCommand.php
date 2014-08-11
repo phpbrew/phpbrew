e2421ee8d19034e8afc5776d5d73bc140b68e8e4
Márcio Almada <marcio3w@gmail.com>
2014-08-11 13:23:23 -0300

    fixe info messages for `phpbrew config` and `phpbrew ext config` commands #284 #285

--- src/PhpBrew/Command/ExtCommand/ConfigCommand.php
+++ src/PhpBrew/Command/ExtCommand/ConfigCommand.php
@@ -15,11 +15,11 @@ class ConfigCommand extends AbstractConfigCommand
     {
         $extension = new Extension($extname, $this->logger);
         $file = $extension->getMeta()->getIniFile();
+        $this->logger->info("Looking for {$file} file...");
         if(! file_exists($file)) {
+            $file .= '.disabled'; // try with ini.disabled file
             $this->logger->info("Looking for {$file} file...");
-            $file .= '.disabled'; // try disabled file
             if(! file_exists($file)) {
-                $this->logger->info("Looking for {$file} file...");
                 $this->logger->warn("Sorry, I can't find the ini file for the requested extension: \"{$extname}\".");
                 return;
             }
