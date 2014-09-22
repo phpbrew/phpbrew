4dfc38a570d43f1f0c439277af164ce770ceda95
Evseev Nikolay <evseevnn@gmail.com>
2014-09-22 17:56:55 +0400

    fix of path on phpbrew github page

--- src/PhpBrew/Exceptions/OopsException.php
+++ src/PhpBrew/Exceptions/OopsException.php
@@ -6,6 +6,6 @@ class OopsException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('Oops, report this issue on GitHub? http://github.com/c9s/phpbrew ');
+        parent::__construct('Oops, report this issue on GitHub? http://github.com/phpbrew/phpbrew ');
     }
 }
