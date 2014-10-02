01d69e8954b569232a8d8fa6c7265251ac1f2ed6
Márcio Almada <marcio3w@gmail.com>
2014-10-02 18:45:36 -0300

    Checking in changes prior to tagging of version 1.14.2

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -6,7 +6,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = "1.14.1";
+    const VERSION = "1.14.2";
 
     public function init()
     {
