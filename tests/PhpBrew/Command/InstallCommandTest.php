2f26fc7eac4c4ee90c530d751d2e2f1b7c39d802
c9s <yoanlin93@gmail.com>
2014-10-05 13:01:46 +0800

    implement setupApplication method

--- tests/PhpBrew/Command/InstallCommandTest.php
+++ tests/PhpBrew/Command/InstallCommandTest.php
@@ -3,6 +3,12 @@ use CLIFramework\Testing\CommandTestCase;
 
 class InstallCommandTest extends CommandTestCase
 {
+
+    public function setupApplication() {
+        return new PhpBrew\Console;
+    }
+
+
     public function testInstallCommand()
     {
         $ret = $this->runCommand('phpbrew install 5.4 +default');
