0536c6ac536389f45393874039c22d4efe2079c9
c9s <yoanlin93@gmail.com>
2014-10-06 17:57:26 +0800

    Add Version class to handle version convertion

---
+++ src/PhpBrew/Version.php
@@ -0,0 +1,44 @@
+<?php
+namespace PhpBrew;
+
+/**
+ * Version class to handle version conversions:
+ *
+ *   - 5.3 => php-5.3.29 (get the latest patched version)
+ *   - 5.3.29 => php-5.3.29
+ *   - php-5.4 => php-5.4.26
+ *
+ */
+class Version
+{
+    public $version;
+
+    public $dist = 'php'; // can be hhvm
+
+    public function __construct($a, $dist = NULL)
+    {
+        if (preg_match('/^(\w+)-(.*?)$/',$a, $regs)) {
+            $this->dist = $dist ?: $regs[1];
+            $this->version = $version;
+        } else {
+            $this->version = $a;
+            $this->dist = $dist ?: 'php'; // default dist name to PHP
+        }
+    }
+
+    public function compare($b) {
+        // TODO:
+    }
+
+    /**
+     * @return string the version string, php-5.3.29, php-5.4.2 without prefix
+     */
+    public function getCanonicalizedVersionName() {
+        return $this->dist . '-' . $this->version;
+    }
+
+}
+
+
+
+
