3955757b9c8d6c12f9a7b154c5ea0b78709e15c9
c9s <yoanlin93@gmail.com>
2014-10-09 10:41:58 +0800

    Remove release manager

--- src/PhpBrew/Command/KnownCommand.php
+++ src/PhpBrew/Command/KnownCommand.php
@@ -20,13 +20,7 @@ class KnownCommand extends \CLIFramework\Command
     public function options($opts)
     {
         $opts->add('more', 'show more older versions');
-        $opts->add('svn', 'list subversion phps');
         $opts->add('old', 'list old phps (less than 5.3)');
-        $managers = PhpSource::getReleaseManagers();
-
-        foreach ($managers as $id => $fullName) {
-            $opts->add($id, "list $id phps");
-        }
     }
 
     public function execute()
@@ -47,30 +41,6 @@ class KnownCommand extends \CLIFramework\Command
                 array_splice($versions, 8);
             }
             echo $this->formatter->format("{$minorVersion}+\t", 'yellow'), join(', ', $versions), "\n";
-        }
-
-        if ($this->options->svn) {
-            $svnVersions = \PhpBrew\PhpSource::getSvnVersions();
-            echo $this->formatter->format("Available svn versions:\n", 'yellow');
-
-            foreach ($svnVersions as $version => $arg) {
-                echo "  " . $version . "\n";
-            }
-        }
-
-        $managers = PhpSource::getReleaseManagers();
-        foreach ($managers as $id => $fullName) {
-            if ($this->options->$id) {
-                $versions = \PhpBrew\PhpSource::getReleaseManagerVersions($id);
-                echo $this->formatter->format(
-                    "Available versions from PHP Release Manager: $fullName\n",
-                    'yellow'
-                );
-
-                foreach ($versions as $version => $arg) {
-                    echo "  " . $version . "\n";
-                }
-            }
         }
     }
 }
