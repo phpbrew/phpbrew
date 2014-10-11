621bef7a6ece68153365faa8c07ec539adc30cd1
c9s <yoanlin93@gmail.com>
2014-10-11 15:19:48 +0800

    Add MakeCleanTask

---
+++ src/PhpBrew/Tasks/MakeCleanTask.php
@@ -0,0 +1,24 @@
+<?php
+namespace PhpBrew\Tasks;
+use PhpBrew\Build;
+use PhpBrew\Config;
+
+/**
+ * Task to run `make clean`
+ */
+class MakeCleanTask extends BaseTask
+{
+    public function clean(Build $build) {
+        return $this->_clean($build->getSourceDirectory());
+    }
+
+    public function _clean($path)
+    {
+        if (!file_exists($path . DIRECTORY_SEPARATOR . 'Makefile')) {
+            return false;
+        }
+        $this->logger->info('===> Cleaning...');
+        system("make -C $path clean");
+        return true;
+    }
+}
