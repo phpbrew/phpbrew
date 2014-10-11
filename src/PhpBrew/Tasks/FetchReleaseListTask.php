b3091ded4339d8a9d2504961470ff1cc3f2ac9c6
c9s <yoanlin93@gmail.com>
2014-10-11 17:20:22 +0800

    Add FetchReleaseListTask

---
+++ src/PhpBrew/Tasks/FetchReleaseListTask.php
@@ -0,0 +1,27 @@
+<?php
+namespace PhpBrew\Tasks;
+use CurlKit\CurlDownloader;
+use CurlKit\ReleaseList;
+use CurlKit\Progress\ProgressBar;
+use PhpBrew\Config;
+use Exception;
+
+class FetchReleaseListTask extends BaseTask
+{
+    public function fetch($branch = 'feature/release-list') {
+        $this->logger->info('===> Fetching release list...');
+        $downloader = new CurlDownloader;
+        $downloader->setProgressHandler(new ProgressBar);
+        $url = "https://raw.githubusercontent.com/phpbrew/phpbrew/$branch/assets/php-releases.json";
+        $json = $downloader->request($url);
+        $localFilepath = Config::getPHPReleaseListPath();
+        if (false === file_put_contents($localFilepath, $json)) {
+            throw new Exception("Can't store release json file");
+        }
+        $this->logger->info('===> Release list downloaded');
+    }
+
+}
+
+
+
