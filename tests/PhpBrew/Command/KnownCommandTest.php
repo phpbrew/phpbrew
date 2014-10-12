375adab714a41da6d181c4005cca5ca009155f64
Márcio Almada <marcio3w@gmail.com>
2014-10-12 04:12:04 -0300

    use @outputBuffering enabled for KnownCommandTest

--- tests/PhpBrew/Command/KnownCommandTest.php
+++ tests/PhpBrew/Command/KnownCommandTest.php
@@ -8,15 +8,16 @@ class KnownCommandTest extends CommandTestCase
         return new PhpBrew\Console;
     }
 
+    /**
+     * @outputBuffering enabled
+     */
     public function testKnownCommand()
     {
-        ob_start();
         $this->assertTrue($this->runCommand('phpbrew known --update'));
         $this->assertTrue($this->runCommand('phpbrew known -u'));
         $this->assertTrue($this->runCommand('phpbrew known'));
         $this->assertTrue($this->runCommand('phpbrew known --more'));
         $this->assertTrue($this->runCommand('phpbrew known --old --more'));
-        ob_end_clean();
     }
 }
 
