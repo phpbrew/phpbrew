6dbafb59eb21cc2b96769ffecb521793c688c40f
Márcio Almada <marcio3w@gmail.com>
2014-10-11 23:48:28 -0300

    use strict tests for VersionTest

--- tests/PhpBrew/VersionTest.php
+++ tests/PhpBrew/VersionTest.php
@@ -8,18 +8,16 @@ class VersionTest extends PHPUnit_Framework_TestCase
         $versionA = new Version('php-5.4.22');
         $versionB = new Version('5.4.22', 'php');
 
-        is('php-5.4.22',$versionA->getCanonicalizedVersionName());
-        is('php-5.4.22',$versionB->getCanonicalizedVersionName());
-
-        is('5.4.22',$versionA->getVersion());
-        is('5.4.22',$versionB->getVersion());
-
-        is($versionA->__toString(), $versionB->__toString());
+        $this->assertEquals('php-5.4.22',$versionA->getCanonicalizedVersionName());
+        $this->assertEquals('php-5.4.22',$versionB->getCanonicalizedVersionName());
+        $this->assertEquals('5.4.22',$versionA->getVersion());
+        $this->assertEquals('5.4.22',$versionB->getVersion());
+        $this->assertEquals($versionA->__toString(), $versionB->__toString());
     }
 
     public function testFindLastPatchVersion() {
         $ret = Version::findLatestPatchVersion('5.5', array('5.5.26', '5.5.25', '5.5.1'));
-        is('5.5.26',$ret);
+        $this->assertEquals('5.5.26',$ret);
     }
 
     public function testHasPatchVersion() {
