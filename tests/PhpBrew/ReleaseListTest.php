4dd19f9756a2e65be2bf5d84fec704aa9d0eb550
Márcio Almada <marcio3w@gmail.com>
2014-10-12 00:52:41 -0300

    fix ReleaseListTest

--- tests/PhpBrew/ReleaseListTest.php
+++ tests/PhpBrew/ReleaseListTest.php
@@ -14,17 +14,26 @@ class ReleaseListTest extends PHPUnit_Framework_TestCase
     public function testGetVersions()
     {
         $versions = $this->releaseList->getVersions("5.3");
-        ok($versions);
-        ok(is_array($versions));
+        $this->assertSame(
+            $versions['5.3.0'],
+            array(
+                'version' => "5.3.0",
+                'announcement' => "http://php.net/releases/5_3_0.php",
+                'date' => "30 June 2009",
+                'filename' => "php-5.3.0.tar.bz2",
+                'md5' => "846760cd655c98dfd86d6d97c3d964b0",
+                'name' => "PHP 5.3.0 (tar.bz2)",
+            )
+        );
     }
 
 
     public function versionDataProvider() {
         return array(
-            array("5.3"),
-            array("5.4"),
-            array("5.5"),
-            array("5.6"),
+            array("5.3", "5.3.29"),
+            array("5.4", "5.4.33"),
+            array("5.5", "5.5.17"),
+            array("5.6", "5.6.1"),
         );
     }
 
@@ -34,9 +43,8 @@ class ReleaseListTest extends PHPUnit_Framework_TestCase
     public function testLatestPatchVersion($major, $minor)
     {
         $version = $this->releaseList->getLatestPatchVersion($major, $minor);
-        ok($version);
-        ok($version['filename']);
-        ok($version['md5']);
+        $this->assertInternalType('array', $version);
+        $this->assertEquals($version['version'], $minor);
     }
 
 }
