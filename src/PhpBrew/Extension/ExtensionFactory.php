985ad54ff118bd0d61acda431f88f48a901696d7
c9s <yoanlin93@gmail.com>
2014-10-29 21:38:06 +0800

    Add createFromDirectory method for createing extension object

--- src/PhpBrew/Extension/ExtensionFactory.php
+++ src/PhpBrew/Extension/ExtensionFactory.php
@@ -12,6 +12,48 @@ use PEARX\PackageXml\Parser as PackageXmlParser;
  */
 class ExtensionFactory
 {
+    static public function createFromDirectory($packageName, $extensionDir) {
+        $packageXmlPath = $extensionDir . '/package.xml';
+
+        // If the package.xml exists, we may get the configureoptions for configuring the Makefile 
+        // and use the provided extension name to enable the extension.
+        //
+        // Currently only PECL extensions have package.xml, however It's the 
+        // best strategy to install the extension.
+        if (file_exists($packageXmlPath)) {
+            // $this->logger->warning("===> Using xml extension meta");
+            return self::createPeclExtension($packageName, $packageXmlPath);
+        }
+
+        // If the config.m4 or config0.m4 exists, it requires us to run `phpize` to 
+        // initialize the `configure` script.
+        //
+        // It's basically a fallback for extensions that don't have package.xml.
+        // Generlly, The possible extensions using this strategy are usually 
+        // PHP's core extensions, which are shipped in the distribution file.
+        $configM4Path = $extensionDir . '/config.m4';
+        if (file_exists($configM4Path)) {
+            // $this->logger->warning("===> Using m4 extension meta");
+            return self::createM4Extension($packageName, $configM4Path);
+        }
+
+        // quote:
+        //   the 0 there makes sure it gets into another stage of the buildprocess, the
+        //   top IIRC, it was added @ 12th May 2001, 12:09am (10 months ago).
+        //
+        // http://grokbase.com/t/php/php-dev/023cpdc9k6/quick-summary-of-changes
+        //
+        // When config[0-9].m4 found, it might be an extension that can't be 
+        // installed as a shared extension. We will need to raise a warning 
+        // message for users.
+        for ($i = 0 ; $i < 10 ; $i++ ) {
+            $configM4Path = $extensionDir . "/config{$i}.m4";
+            if (file_exists($configM4Path)) {
+                // $this->logger->warning("===> Using m4 extension meta");
+                return self::createM4Extension($packageName, $configM4Path);
+            }
+        }
+    }
 
 
     static public function lookup($packageName, $lookupDirectories = array(), $fallback = true) 
@@ -24,50 +66,9 @@ class ExtensionFactory
 
         foreach($lookupDirectories as $lookupDir) {
             $extensionDir = $lookupDir . DIRECTORY_SEPARATOR . $packageName;
-            $packageXmlPath = $extensionDir . '/package.xml';
-
-            // If the package.xml exists, we may get the configureoptions for configuring the Makefile 
-            // and use the provided extension name to enable the extension.
-            //
-            // Currently only PECL extensions have package.xml, however It's the 
-            // best strategy to install the extension.
-            if (file_exists($packageXmlPath)) {
-                // $this->logger->warning("===> Using xml extension meta");
-                return self::createPeclExtension($packageName, $packageXmlPath);
+            if ($ext = self::createFromDirectory($packageName, $extensionDir)) {
+                return $ext;
             }
-
-            // If the config.m4 or config0.m4 exists, it requires us to run `phpize` to 
-            // initialize the `configure` script.
-            //
-            // It's basically a fallback for extensions that don't have package.xml.
-            // Generlly, The possible extensions using this strategy are usually 
-            // PHP's core extensions, which are shipped in the distribution file.
-            $configM4Path = $extensionDir . '/config.m4';
-            if (file_exists($configM4Path)) {
-                // $this->logger->warning("===> Using m4 extension meta");
-                return self::createM4Extension($packageName, $configM4Path);
-            }
-
-            // quote:
-            //   the 0 there makes sure it gets into another stage of the buildprocess, the
-            //   top IIRC, it was added @ 12th May 2001, 12:09am (10 months ago).
-            //
-            // http://grokbase.com/t/php/php-dev/023cpdc9k6/quick-summary-of-changes
-            //
-            // When config[0-9].m4 found, it might be an extension that can't be 
-            // installed as a shared extension. We will need to raise a warning 
-            // message for users.
-            for ($i = 0 ; $i < 10 ; $i++ ) {
-                $configM4Path = $extensionDir . "/config{$i}.m4";
-                if (file_exists($configM4Path)) {
-                    // $this->logger->warning("===> Using m4 extension meta");
-                    return self::createM4Extension($packageName, $configM4Path);
-                }
-            }
-            /*
-            $meta = new ExtensionMetaPolyfill($packageName);
-            return new Extension1($packageName, $meta);
-            */
         }
     }
 
