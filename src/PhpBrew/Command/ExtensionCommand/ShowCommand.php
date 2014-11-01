0113140191fa64ab1aadcf75286f7dedb282a6ab
c9s <yoanlin93@gmail.com>
2014-11-01 17:47:18 +0800

    Add ext show command to show the details for the extension

--- src/PhpBrew/Command/ExtensionCommand/ShowCommand.php
+++ src/PhpBrew/Command/ExtensionCommand/ShowCommand.php
@@ -1,7 +1,8 @@
 <?php
 namespace PhpBrew\Command\ExtensionCommand;
 use PhpBrew\Config;
-use PhpBrew\Extension;
+use PhpBrew\Extension\Extension;
+use PhpBrew\Extension\M4Extension;
 use PhpBrew\Extension\ExtensionManager;
 use PhpBrew\Extension\ExtensionFactory;
 use PhpBrew\Extension\PeclExtensionInstaller;
@@ -34,6 +35,38 @@ class ShowCommand extends \CLIFramework\Command
             });
     }
 
+    public function describeExtension(Extension $ext) 
+    {
+
+        $info = array(
+            'Name' => $ext->getExtensionName(),
+            'Config' => $ext->getConfigM4Path(),
+            'INI File' => $ext->getConfigFilePath(),
+            'Extension Type'    => ($ext instanceof PeclExtension) ? 'Pecl extension' : 'Core extension',
+            'Zend'              => $ext->isZend() ? 'yes' : 'no',
+            'Loaded' => (extension_loaded($ext->getExtensionName()) ? 'yes' : 'no'),
+        );
+
+        foreach($info as $label => $val) {
+            $this->logger->writeln(sprintf('%20s: %s', $label, $val));
+        }
+
+        if ($ext instanceof M4Extension) {
+            $options = $ext->getConfigureOptions();
+            if (!empty($options)) {
+                $this->logger->newline();
+                $this->logger->writeln(sprintf('%20s: ', 'Configure Options'));
+                $this->logger->newline();
+                foreach($options as $option) {
+                    $this->logger->writeln(sprintf('        %-32s %s', 
+                            $option->option . ($option->valueHint ? '[=' . $option->valueHint . ']' : ''),
+                            $option->desc));
+                    $this->logger->newline();
+                }
+            }
+        }
+    }
+
 
     public function execute($extensionName)
     {
@@ -52,7 +85,7 @@ class ShowCommand extends \CLIFramework\Command
         if (!$ext) {
             throw new Exception("$extensionName not found.");
         }
-        var_dump( $ext );
+        $this->describeExtension($ext);
     }
 }
 
