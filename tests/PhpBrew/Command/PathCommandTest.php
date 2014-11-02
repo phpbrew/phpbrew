dfcd76945216178aa593e8b8571996fe50a948e5
c9s <yoanlin93@gmail.com>
2014-11-02 22:50:52 +0800

    Update test suite definitions

---
+++ tests/PhpBrew/Command/PathCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+use CLIFramework\Testing\CommandTestCase;
+
+class PathCommandTest extends CommandTestCase
+{
+
+    public function setupApplication() {
+        return new PhpBrew\Console;
+    }
+
+    public function setUp() {
+        parent::setUp();
+        putenv('PHPBREW_HOME=' . getcwd() . '/.phpbrew');
+        putenv('PHPBREW_ROOT=' . getcwd() . '/.phpbrew');
+    }
+
+    public function argumentsProvider() {
+
+        return array( 
+            array("build"),
+            array("ext-src"),
+            array("ext"),
+            array("include"),
+            array("etc"),
+            array("dist"),
+            array("root"),
+            array("home"),
+        );
+    }
+
+    /**
+     * @outputBuffering enabled
+     * @dataProvider argumentsProvider
+     */
+    public function testInstallCommand($arg) {
+        ob_start();
+        $this->assertTrue($this->runCommand("phpbrew path $arg"));
+        ob_end_clean();
+    }
+
+
+}
