b92023f822cff748a33dcf4fa61a7ba9eab1b065
c9s <yoanlin93@gmail.com>
2014-11-02 23:48:40 +0800

    More install command tests

--- tests/PhpBrew/Command/InstallCommandTest.php
+++ tests/PhpBrew/Command/InstallCommandTest.php
@@ -7,7 +7,7 @@ class InstallCommandTest extends CommandTestCase
      * @outputBuffering enabled
      */
     public function testInstallCommandLatestMinorVersion() {
-        $this->assertTrue($this->runCommand('phpbrew --quiet install -d 5.4'));
+        $this->assertTrue($this->runCommand("phpbrew --quiet install 5.4")); // we will likely get 5.4.34 - 2014-11-02
     }
 
     /**
@@ -15,8 +15,29 @@ class InstallCommandTest extends CommandTestCase
      */
     public function testInstallCommand()
     {
-        $this->assertTrue($this->runCommand('phpbrew --quiet install -d 5.4.29'));
-        $this->assertTrue($this->runCommand('phpbrew --quiet install -d --like 5.4.29 5.5 +soap'));
+        $this->assertTrue($this->runCommand("phpbrew --quiet install 5.4.29 +sqlite +intl +icu"));
     }
+
+    /**
+     * @outputBuffering enabled
+     * @depends testInstallCommand
+     */
+    public function testCleanCommand()
+    {
+        $this->assertTrue($this->runCommand("phpbrew --quiet clean 5.4.29"));
+    }
+
+
+    /**
+     * @outputBuffering enabled
+     * @depends testInstallCommand
+     */
+    public function testInstallLikeCommand() {
+        $this->assertTrue($this->runCommand("phpbrew --quiet install -d --like 5.4.29 5.5.18 +soap"));
+    }
+
+
+
+
 }
 
