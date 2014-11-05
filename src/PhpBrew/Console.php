55420d1ae1de04b3849d738318a5c96e227d909f
c9s <yoanlin93@gmail.com>
2014-11-05 10:48:13 +0800

    Checking in changes prior to tagging of version 1.16.6.

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -6,7 +6,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = "1.16.5";
+    const VERSION = "1.16.6";
 
     public function options($opts) {
         parent::options($opts);
