eafb877b8e6c2e4030d2d40fdaf66cfa46ca30dd
Márcio Almada <marcio3w@gmail.com>
2014-11-23 20:45:26 -0300

    remove unnecessary ob_start and ob_end_clean call from InstallCommandTest.php

--- tests/PhpBrew/Command/DownloadCommandTest.php
+++ tests/PhpBrew/Command/DownloadCommandTest.php
@@ -20,12 +20,8 @@ class DownloadCommandTest extends CommandTestCase
      * @dataProvider versionDataProvider
      */
     public function testDownloadCommand($versionName) {
-        ob_start();
         $this->assertTrue($this->runCommand("phpbrew -q download $versionName"));
         $this->assertTrue($this->runCommand("phpbrew -q download $versionName")); // redownload should just check the checksum instead of extracting it.
         $this->assertTrue($this->runCommand("phpbrew -q download -f $versionName"));
-        ob_end_clean();
     }
-
-
 }
