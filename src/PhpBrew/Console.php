532e65fe476c9f09cdb1905bdea84ed2f998ebcd
c9s <yoanlin93@gmail.com>
2014-12-07 23:12:50 +0800

    Checking in changes prior to tagging of version 1.18.0.

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -6,7 +6,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = "1.17.2";
+    const VERSION = "1.18.0";
 
     public function options($opts) {
         parent::options($opts);
