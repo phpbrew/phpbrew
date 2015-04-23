d5753cb3c41eb38b8ff9a51b4f4d3e588162a0ed
Márcio Almada <marcio3w@gmail.com>
2015-04-23 08:29:05 -0300

    clean up scripts dir and encourage use of `make *` instead

--- scripts/test-ext-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-php bin/phpbrew --debug ext install openssl
-php bin/phpbrew --debug ext install opcache
-php bin/phpbrew --debug ext install xdebug
-php bin/phpbrew --debug ext install soap
