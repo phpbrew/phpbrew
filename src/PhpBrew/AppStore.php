706b1881ce2d7c6167487f89c7e8657462173c0f
c9s <yoanlin93@gmail.com>
2015-07-16 00:46:02 +0800

    app command: add box-2.5

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -14,7 +14,7 @@ class AppStore
             'sami'      => array('url' => 'http://get.sensiolabs.org/sami.phar', 'as' => 'sami'),
             'phpcs'     => array('url' => 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar', 'as' => 'phpcs'),
             'onion'     => array('url' => 'https://raw.githubusercontent.com/phpbrew/Onion/master/onion', 'as' => 'onion'),
-            
+            'box-2.5'   => array('url' => 'https://github.com/box-project/box2/releases/download/2.5.2/box-2.5.2.phar', 'as' => 'box'),
         );
         $phpunitapps = explode(' ','phpunit phpcov phpcpd phpdcd phptok phploc');
         foreach ($phpunitapps as $phpunitapp) {
