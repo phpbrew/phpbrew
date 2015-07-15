74ba44705540cd6fecc7d13b21790350c9f65917
c9s <yoanlin93@gmail.com>
2015-07-16 00:34:28 +0800

    Add behat 2.5 and 3.0

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -6,10 +6,11 @@ class AppStore
     public static function all() 
     {
         $apps = array(
-            'composer' => array('url' => 'https://getcomposer.org/composer.phar', 'as' => 'composer'),
-            'phpunit' => array('url' => 'https://phar.phpunit.de/phpunit.phar', 'as' => 'phpunit'),
-            'phpmd' => array('url' => 'http://static.phpmd.org/php/latest/phpmd.phar', 'as' => 'phpmd'),
-            'behat' => array('url' => 'https://github.com/Behat/Behat/releases/download/v2.5.5/behat.phar', 'as' => 'behat'),
+            'composer'  => array('url' => 'https://getcomposer.org/composer.phar', 'as'                              => 'composer'),
+            'phpunit'   => array('url' => 'https://phar.phpunit.de/phpunit.phar', 'as'                               => 'phpunit'),
+            'phpmd'     => array('url' => 'http://static.phpmd.org/php/latest/phpmd.phar', 'as'                      => 'phpmd'),
+            'behat-2.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v2.5.5/behat.phar', 'as' => 'behat'),
+            'behat-3.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v3.0.15/behat.phar', 'as' => 'behat'),
         );
         $phpunitapps = explode(' ','phpunit phpcov phpcpd phpdcd phptok phploc');
         foreach ($phpunitapps as $phpunitapp) {
