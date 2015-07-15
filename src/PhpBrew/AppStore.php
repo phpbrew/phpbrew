fdaaf7229ec6e3d6589257cf842c2b2cf87535a1
c9s <yoanlin93@gmail.com>
2015-07-16 00:33:16 +0800

    Add behat

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -9,6 +9,7 @@ class AppStore
             'composer' => array('url' => 'https://getcomposer.org/composer.phar', 'as' => 'composer'),
             'phpunit' => array('url' => 'https://phar.phpunit.de/phpunit.phar', 'as' => 'phpunit'),
             'phpmd' => array('url' => 'http://static.phpmd.org/php/latest/phpmd.phar', 'as' => 'phpmd'),
+            'behat' => array('url' => 'https://github.com/Behat/Behat/releases/download/v2.5.5/behat.phar', 'as' => 'behat'),
         );
         $phpunitapps = explode(' ','phpunit phpcov phpcpd phpdcd phptok phploc');
         foreach ($phpunitapps as $phpunitapp) {
