a7335e339b104f0c13ac318077f2b53175e68ab3
c9s <yoanlin93@gmail.com>
2015-07-16 00:38:26 +0800

    app command: add phpcs

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -12,6 +12,7 @@ class AppStore
             'behat-2.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v2.5.5/behat.phar', 'as' => 'behat'),
             'behat-3.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v3.0.15/behat.phar', 'as' => 'behat'),
             'sami'      => array('url' => 'http://get.sensiolabs.org/sami.phar', 'as' => 'sami'),
+            'phpcs'     => array('url' => 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar', 'as' => 'phpcs'),
         );
         $phpunitapps = explode(' ','phpunit phpcov phpcpd phpdcd phptok phploc');
         foreach ($phpunitapps as $phpunitapp) {
