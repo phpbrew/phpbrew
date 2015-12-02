3848399bceb7ed5d8820dd0b2d537a4d2933ca6a
c9s <yoanlin93@gmail.com>
2015-12-03 07:06:48 +0800

    Pass build object to SystemCommandException

--- src/PhpBrew/Exception/SystemCommandException.php
+++ src/PhpBrew/Exception/SystemCommandException.php
@@ -1,20 +1,29 @@
 <?php
 namespace PhpBrew\Exception;
 use RuntimeException;
+use PhpBrew\Build;
 
 class SystemCommandException extends RuntimeException
 {
     protected $logFile;
 
-    public function __construct($message, $logFile = null)
+    protected $build;
+
+    public function __construct($message, Build $build = null, $logFile = null)
     {
         parent::__construct($message);
+        $this->build = $build;
         $this->logFile = $logFile;
     }
 
     public function getLogFile()
     {
         return $this->logFile;
+    }
+
+    public function getBuild()
+    {
+        return $this->build;
     }
 }
 
