4399a6d5fe62136fb57dae0da531395a52cac00b
c9s <yoanlin93@gmail.com>
2015-12-05 19:01:10 +0800

    show last 5 lines of log file only if the file exists

--- src/PhpBrew/Command/InstallCommand.php
+++ src/PhpBrew/Command/InstallCommand.php
@@ -400,15 +400,17 @@ class InstallCommand extends Command
             $buildLog = $e->getLogFile();
             $this->logger->error("Error: " . $e->getMessage());
             $this->logger->error("Configure options: " . join(' ', $configureOptions));
-            $this->logger->error("Last 5 lines in the log file:");
 
-            $lines = array_slice(file($buildLog), -5);
-            foreach ($lines as $line) {
-                echo $line , PHP_EOL;
+
+            if (file_exists($buildLog)) {
+                $this->logger->error("Last 5 lines in the log file:");
+                $lines = array_slice(file($buildLog), -5);
+                foreach ($lines as $line) {
+                    echo $line , PHP_EOL;
+                }
+                $this->logger->error("Please checkout the build log file for more details:");
+                $this->logger->error("\t tail $buildLog");
             }
-
-            $this->logger->error("Please checkout the build log file for more details:");
-            $this->logger->error("\t tail $buildLog");
             return;
         }
 
