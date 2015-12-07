c28f78bf2adb3c5855c7f9fa183741f4599bcafe
c9s <yoanlin93@gmail.com>
2015-12-08 00:01:04 +0800

    bump version to 1.20.3

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -6,7 +6,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = "1.20.2";
+    const VERSION = "1.20.3";
 
     public function options($opts) {
         parent::options($opts);
