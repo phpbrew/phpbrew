d5bbc5eab6f44148c44f571888fc63b73f8b27a8
c9s <yoanlin93@gmail.com>
2016-04-28 23:49:23 +0800

    do not show warnings if TRAVIS is set

--- src/PhpBrew/Command/UseCommand.php
+++ src/PhpBrew/Command/UseCommand.php
@@ -40,6 +40,8 @@ class UseCommand extends Command
             putenv("PHPBREW_PATH=$root/php/$buildName/bin");
             putenv("PHPBREW_BIN=$home/bin");
         }
-        $this->logger->warning("You should not see this, if you see this, it means you didn't load the ~/.phpbrew/bashrc script, please check if bashrc is sourced in your shell.");
+        if (!getenv('TRAVIS')) {
+            $this->logger->warning("You should not see this, if you see this, it means you didn't load the ~/.phpbrew/bashrc script, please check if bashrc is sourced in your shell.");
+        }
     }
 }
