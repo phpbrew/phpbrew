0c63aaa25c28ee8c071720882de2324cf67bd04a
c9s <yoanlin93@gmail.com>
2016-06-13 18:13:15 +0800

    Release 1.22.4

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -14,7 +14,7 @@ use PhpBrew\Exception\SystemCommandException;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = '1.22.3';
+    const VERSION = '1.22.4';
 
     public function options($opts)
     {
