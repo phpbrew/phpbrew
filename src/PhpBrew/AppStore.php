3eeebe90ca3949b3558da64b8c93b9bd45cd4827
Peter Aba <peter@peteraba.com>
2017-03-12 13:51:28 +0100

    App store update

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -11,12 +11,12 @@ class AppStore
             'phpunit' => array('url' => 'https://phar.phpunit.de/phpunit.phar', 'as' => 'phpunit'),
             'phpmd' => array('url' => 'http://static.phpmd.org/php/latest/phpmd.phar', 'as' => 'phpmd'),
             'behat-2.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v2.5.5/behat.phar', 'as' => 'behat'),
-            'behat-3.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v3.0.15/behat.phar', 'as' => 'behat'),
+            'behat-3.3' => array('url' => 'https://github.com/Behat/Behat/releases/download/v3.3.0/behat.phar', 'as' => 'behat'),
             'sami'      => array('url' => 'http://get.sensiolabs.org/sami.phar',                    'as' => 'sami'),
             'phpcs'     => array('url' => 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar', 'as' => 'phpcs'),
             'pdepend'   => array('url' => 'http://static.pdepend.org/php/latest/pdepend.phar',      'as' => 'pdepend'),
             'onion'     => array('url' => 'https://raw.githubusercontent.com/phpbrew/Onion/master/onion', 'as' => 'onion'),
-            'box-2.5'   => array('url' => 'https://github.com/box-project/box2/releases/download/2.5.2/box-2.5.2.phar', 'as' => 'box'),
+            'box-2.7'   => array('url' => 'https://github.com/box-project/box2/releases/download/2.7.5/box-2.7.5.phar', 'as' => 'box'),
             'psysh'     => array('url' => 'https://git.io/psysh', 'as' => 'psysh'),
         );
         $phpunitapps = explode(' ', 'phpunit phpcov phpcpd phpdcd phptok phploc');
