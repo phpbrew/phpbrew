ecdd82190d3bcf37b8957e6771a4bbaa3f2cfed7
Ikuo Degawa <degawa@ikuwow.com>
2017-03-20 18:16:55 +0900

    Add phpcbf

--- src/PhpBrew/AppStore.php
+++ src/PhpBrew/AppStore.php
@@ -14,6 +14,7 @@ class AppStore
             'behat-3.5' => array('url' => 'https://github.com/Behat/Behat/releases/download/v3.0.15/behat.phar', 'as' => 'behat'),
             'sami'      => array('url' => 'http://get.sensiolabs.org/sami.phar',                    'as' => 'sami'),
             'phpcs'     => array('url' => 'https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar', 'as' => 'phpcs'),
+            'phpcbf'    => array('url' => 'https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar', 'as' => 'phpcbf'),
             'pdepend'   => array('url' => 'http://static.pdepend.org/php/latest/pdepend.phar',      'as' => 'pdepend'),
             'onion'     => array('url' => 'https://raw.githubusercontent.com/phpbrew/Onion/master/onion', 'as' => 'onion'),
             'box-2.5'   => array('url' => 'https://github.com/box-project/box2/releases/download/2.5.2/box-2.5.2.phar', 'as' => 'box'),
