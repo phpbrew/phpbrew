7484e69ec87612d16a58ab0b8d48a9c5eaf42a14
Kevin Perez <kevindperezm@gmail.com>
2018-10-28 00:11:06 -0500

    install php-vcr
    This installs php-vcr into the project. php-vcr is a port of a popular ruby gem
    that allows to record and reproduce http interactions, thus avoiding the need to
    actually hit web services during tests.
    
    More info at http://php-vcr.github.io

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,11 @@
 <?php
+use VCR\VCR;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+VCR::configure()
+  ->setCassettePath('tests/fixtures/vcr_cassettes')
+  ->enableLibraryHooks(array('curl'))
+  ->setStorage('json');
+
+VCR::turnOn();
