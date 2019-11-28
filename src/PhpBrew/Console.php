a3745f86f827a8c72d0958519b45a5016f88a91f
Sergei Morozov <morozov@tut.by>
2019-11-28 08:59:35 -0800

    Release 1.24.1

--- src/PhpBrew/Console.php
+++ src/PhpBrew/Console.php
@@ -14,7 +14,7 @@ use PhpBrew\Exception\SystemCommandException;
 class Console extends Application
 {
     const NAME = 'phpbrew';
-    const VERSION = '1.24.0';
+    const VERSION = '1.24.1';
 
     public function options($opts)
     {
