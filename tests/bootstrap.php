92b18540505d02ec490ca8b9e5cd3bcdf07f382d
Sergei Morozov <morozov@tut.by>
2019-12-11 10:38:50 -0800

    PHPUnit 6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,8 @@ use VCR\VCR;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+class_alias('PHPUnit\\Framework\\TestCase', 'PHPUnit_Framework_TestCase');
+
 VCR::configure()
   ->setCassettePath('tests/fixtures/vcr_cassettes')
   ->enableLibraryHooks(array('curl', 'stream_wrapper'))
