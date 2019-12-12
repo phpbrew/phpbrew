cf4b2a02809e25a4f026aabd4f5aa6e9273abb1a
Sergei Morozov <morozov@tut.by>
2019-12-12 09:42:33 -0800

    Made all patches part of the PHPBrew source tree
    As of now, all patches should be included in the source tree to guarantee integrity. The checksum functionality is removed as no longer needed.
    
    The patch sources are stored alongside the classes they are used by for better cohesion. Additionally, storing them as separate files would require creating a copy before applying anyways because the `patch` command wouldn't be able to use a file from within a PHAR. This issue is avoided by passing the patch to the CLI command via STDIN.

--- src/PhpBrew/Patches/FreeTypePatch.php
+++ src/PhpBrew/Patches/FreeTypePatch.php
@@ -24,14 +24,54 @@ class FreeTypePatch extends Patch
         return $build->isEnabledVariant('gd') && version_compare($build->getVersion(), '7.4', '<=');
     }
 
+    /**
+     * @link https://git.archlinux.org/svntogit/packages.git/commit/?id=65fd0dc1649537b7fd1d539b769251aacec88719
+     */
     public function rules()
     {
         return array(
-            DiffPatchRule::from(
-                'https://git.archlinux.org/svntogit/packages.git/plain/trunk/freetype.patch?h=packages/php'
-            )
-                ->strip(1)
-                ->sha256('07c4648669dc05afc3c1ad5a4739768079c423b817eabf5296ca3d1ea5ffd163')
+            DiffPatchRule::fromPatch(
+                <<<'EOP'
+diff -u -r php-7.2.5/ext/gd/config.m4 php-7.2.5-freetype/ext/gd/config.m4
+--- php-7.2.5/ext/gd/config.m4	2018-04-24 17:09:54.000000000 +0200
++++ php-7.2.5-freetype/ext/gd/config.m4	2018-05-09 14:49:03.647108948 +0200
+@@ -186,6 +186,9 @@
+ AC_DEFUN([PHP_GD_FREETYPE2],[
+   if test "$PHP_FREETYPE_DIR" != "no"; then
+ 
++    AC_PATH_PROG(PKG_CONFIG, pkg-config, no)
++
++    AC_MSG_CHECKING([for freetype])
+     for i in $PHP_FREETYPE_DIR /usr/local /usr; do
+       if test -f "$i/bin/freetype-config"; then
+         FREETYPE2_DIR=$i
+@@ -194,13 +197,20 @@
+       fi
+     done
+ 
+-    if test -z "$FREETYPE2_DIR"; then
++    if test -n "$FREETYPE2_CONFIG"; then
++      FREETYPE2_CFLAGS=`$FREETYPE2_CONFIG --cflags`
++      FREETYPE2_LIBS=`$FREETYPE2_CONFIG --libs`
++      AC_MSG_RESULT([found in $FREETYPE2_DIR])
++    elif test "$PKG_CONFIG" != "no" && $PKG_CONFIG --exists freetype2; then
++      FREETYPE2_DIR=pkg-config
++      FREETYPE2_CFLAGS=`$PKG_CONFIG freetype2 --cflags`
++      FREETYPE2_LIBS=`$PKG_CONFIG freetype2 --libs`
++      AC_MSG_RESULT([found by pkg-config])
++    else
++      AC_MSG_RESULT([not found])
+       AC_MSG_ERROR([freetype-config not found.])
+     fi
+ 
+-    FREETYPE2_CFLAGS=`$FREETYPE2_CONFIG --cflags`
+-    FREETYPE2_LIBS=`$FREETYPE2_CONFIG --libs`
+-
+     PHP_EVAL_INCLINE($FREETYPE2_CFLAGS)
+     PHP_EVAL_LIBLINE($FREETYPE2_LIBS, GD_SHARED_LIBADD)
+     AC_DEFINE(HAVE_LIBFREETYPE,1,[ ])
+EOP
+            )->strip(1)
         );
     }
 }
